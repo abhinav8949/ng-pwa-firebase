1aaa68c5713aec9c0f034a0da8583c13d7b3f4c6
abhinavamar <abhinavday@gmail.com>
2025-03-21 17:33:38 +0530

    pwa-4: Adding required component.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from "../../layout/sidebar/sidebar.component";
-import { BudgetComponent } from "../budget/budget.component";
-import { FooterComponent } from "../../layout/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, SidebarComponent, BudgetComponent, FooterComponent],
+  imports: [RouterModule, SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
